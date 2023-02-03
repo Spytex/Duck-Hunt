fb7c9df494a203898be24dedf7cc97c92bfd4a42
selveresta <artem.lypnyk@softsculptor.net>
2023-02-03 13:48:55 +0200

    create loader class, initialized methods

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -2,4 +2,32 @@
 import pygame
 
 class Loader():
-    pass
+    
+    resources = None
+
+    def __init__(self, resour) -> None:
+        resources = resour
+        print(resources.FPS)
+
+
+    def loadBackGround():
+        pass
+
+
+    def loadSounds():
+        pass
+
+    def loadGuns():
+        pass
+
+
+    def loadMenu():
+        pass
+
+
+    def loadEnemy():    
+        pass
+
+
+    def loadBanners():
+        pass

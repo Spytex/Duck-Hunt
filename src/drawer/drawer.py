8b7c9fd1b440abf4e0a489b4896349fc50940df6
Spytex <ivan.mishko21@gmail.com>
2023-02-03 23:24:49 +0200

    11

--- src/drawer/drawer.py
+++ src/drawer/drawer.py
@@ -1,5 +1,25 @@
-
 import pygame
 
+
 class Drawer():
-    pass
+    def __init__(self):
+        print("hello")
+        pass
+
+    def DrawMenu(self):
+        pass
+
+    def DrawButtons(self):
+        pass
+
+    def DrawLevel(self):
+        pass
+
+    def DrawUnit(self):
+        pass
+
+    def DrawResult(self):
+        pass
+
+    def DrawPause(self):
+        pass

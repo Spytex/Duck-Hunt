8fd0ee7e4333ba40f87b1d545428484d802aaa91
Spytex <ivan.mishko21@gmail.com>
2023-02-07 18:40:03 +0200

    Bug fix, argparse

--- src/resources/resources.py
+++ src/resources/resources.py
@@ -2,15 +2,17 @@ import pygame
 from src.loader import loader
 from src.drawer import button
 from src.drawer import slider
+from src.loader import parser
 
 
 class Resource():
 
     def __init__(self) -> None:
-
         self.load = loader.Loader()
+        self.parser = parser.Parser()
+        self.arg = self.parser.arguments()
 
-        self.FPS = 60
+        self.FPS = self.load.fps
 
         # strings
         self.menu_state = "main"
@@ -19,13 +21,13 @@ class Resource():
         self.imageMenuPath = "assets/menu/"
         self.imageTargetsPath = "assets/targets/"
         self.data = {}
+
         # booleans
-        
         self.run = True
         self.game_paused = True
         self.shot = False
 
-        self.level = 1
+        self.level = self.arg.level
         self.score = 0
         self.bgs = []
 
@@ -52,38 +54,33 @@ class Resource():
         self.enemyImage = pygame.image.load(self.imageTargetsPath + "pig.png").convert_alpha()
 
         # buttons
-        self.resume_button = button.Button(304, 250, self.resume_img, 1)
-        self.options_button = button.Button(297, 375, self.options_img, 1)
-        self.quit_button = button.Button(336, 500, self.quit_img, 1)
-        self.video_button = button.Button(226, 250, self.video_img, 1)
-        self.audio_button = button.Button(225, 375, self.audio_img, 1)
-        self.back_button = button.Button(332, 500, self.back_img, 1)
-        self.fps240_button = button.Button(355, 200, self.font.render('240', True, self.TEXT_COL), 1)
-        self.fps144_button = button.Button(355, 300, self.font.render('144', True, self.TEXT_COL), 1)
-        self.fps60_button = button.Button(365, 400, self.font.render('60', True, self.TEXT_COL), 1)
-        self.sound1_button = button.Button(120, 200, self.font.render('Sound 1', True, self.TEXT_COL), 1)
-        self.sound2_button = button.Button(320, 200, self.font.render('Sound 2', True, self.TEXT_COL), 1)
-        self.sound3_button = button.Button(520, 200, self.font.render('Sound 3', True, self.TEXT_COL), 1)
-
+        self.resume_button = button.Button(self.SCREEN_WIDTH*0.38, self.SCREEN_HEIGHT*0.3125, self.resume_img, 1)
+        self.options_button = button.Button(self.SCREEN_WIDTH*0.37125, self.SCREEN_HEIGHT*0.46875, self.options_img, 1)
+        self.quit_button = button.Button(self.SCREEN_WIDTH*0.42, self.SCREEN_HEIGHT*0.625, self.quit_img, 1)
+        self.video_button = button.Button(self.SCREEN_WIDTH*0.2825, self.SCREEN_HEIGHT*0.3125, self.video_img, 1)
+        self.audio_button = button.Button(self.SCREEN_WIDTH*0.28125, self.SCREEN_HEIGHT*0.46875, self.audio_img, 1)
+        self.back_button = button.Button(self.SCREEN_WIDTH*0.415, self.SCREEN_HEIGHT*0.625, self.back_img, 1)
+        self.fps240_button = button.Button(self.SCREEN_WIDTH*0.44375, self.SCREEN_HEIGHT*0.25, self.font.render('240', True, self.TEXT_COL), 1)
+        self.fps144_button = button.Button(self.SCREEN_WIDTH*0.44375, self.SCREEN_HEIGHT*0.375, self.font.render('144', True, self.TEXT_COL), 1)
+        self.fps60_button = button.Button(self.SCREEN_WIDTH*0.45625, self.SCREEN_HEIGHT*0.5, self.font.render('60', True, self.TEXT_COL), 1)
+        self.sound1_button = button.Button(self.SCREEN_WIDTH*0.15, self.SCREEN_HEIGHT*0.25, self.font.render('Sound 1', True, self.TEXT_COL), 1)
+        self.sound2_button = button.Button(self.SCREEN_WIDTH*0.4, self.SCREEN_HEIGHT*0.25, self.font.render('Sound 2', True, self.TEXT_COL), 1)
+        self.sound3_button = button.Button(self.SCREEN_WIDTH*0.65, self.SCREEN_HEIGHT*0.25, self.font.render('Sound 3', True, self.TEXT_COL), 1)
 
         self.levelSurface = pygame.Surface((self.SCREEN_WIDTH - 50, 50)).convert_alpha()
         # self.levelSurface.fill((0,255,0, 1))
 
-        self.pauseButton = button.Button(30, 740, self.font.render('Pause', True, (255,255,255)), 0.7)
+        self.pauseButton = button.Button(self.SCREEN_WIDTH*0.0375, self.SCREEN_HEIGHT*0.925, self.font.render('Pause', True, (255, 255, 255)), 0.7)
 
         self.yArrayPos = []
         self.countPigs = 10
 
-
         self.grid = []
         self.gridSize = 10
 
-        self.slider = slider.Slider(200, 300, 400, 100)
+        self.slider = slider.Slider(self.SCREEN_WIDTH*0.25, self.SCREEN_HEIGHT*0.375, 400, 100)
 
         self.generateGrid()
-
-
-
 
     def generateGrid(self):
         x = -500
